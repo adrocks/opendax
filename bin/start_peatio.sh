95e1e680829773afedcef9f306f6c05d08c7a8f7
yuknak <yuknak4@gmail.com>
2020-05-06 10:16:06 +0900

    Fix for parity wallets

--- bin/start_peatio.sh
+++ bin/start_peatio.sh
@@ -1,8 +1,16 @@
 #!/bin/bash -x
 
-#bundle exec rake wallet:create['deposit','http://127.0.0.1:8545','changeme']
-#bundle exec rake wallet:create['hot','http://127.0.0.1:8545','changeme']
-#bundle exec rake wallet:create['warm','http://127.0.0.1:8545','changeme']
+# Example for stg1, creating 3 wallets, specify random gen password.
+# that will be strored in app.yml. also generated address will be stored together. 
+# (later, you can change warm wallet to another one in db)
+# this app.yml info are finally automatically put into config/peatio/seed/wallets.yml
+# maybe that is only for the first time
+
+# 10.0.10.13 is cryptosv-stg1
+
+#bundle exec rake wallet:create['deposit','http://10.0.10.13:8545','randomstr1']
+#bundle exec rake wallet:create['hot','http://10.0.10.13:8545','randomstr2']
+#bundle exec rake wallet:create['warm','http://10.0.10.13:8545','randomstr3']
 
 start_opendax() {
   sudo -u deploy bash <<EOS
